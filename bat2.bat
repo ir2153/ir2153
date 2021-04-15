8fef91884ae227ef1b761143bdcae917c86bddeb
ir2153 <82559397+ir2153@users.noreply.github.com>
2021-04-14 21:09:46 -0700

    time waster i made
    coded a time waster using visual basic and batch not very good

---
+++ bat2.bat
@@ -0,0 +1,37 @@
+start chrome.exe
+start
+start notepad.exe
+start chrome.exe
+start
+start notepad.exe
+start chrome.exe
+start
+start notepad.exe
+start chrome.exe
+start
+start notepad.exe
+start chrome.exe
+start
+start notepad.exe
+start chrome.exe
+start
+start notepad.exe
+start chrome.exe
+start
+start notepad.exe
+start chrome.exe
+start
+start notepad.exe
+start chrome.exe
+start
+start notepad.exe
+start chrome.exe
+start
+start notepad.exe
+start chrome.exe
+start
+start notepad.exe
+start chrome.exe
+start
+start notepad.exe
+
